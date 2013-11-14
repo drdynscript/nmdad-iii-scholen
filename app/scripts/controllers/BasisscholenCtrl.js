2bbef99157c3c530f82e8012d35ed087d50d7754
drdynscript <phildp@arteveldehs.be>
2013-11-14 14:07:02 +0100

    ScholenSrvc, BasisscholenCtrl

--- app/scripts/controllers/BasisscholenCtrl.js
+++ app/scripts/controllers/BasisscholenCtrl.js
@@ -4,6 +4,18 @@
     var controllers = angular.module('ddsApp.controllers');
 
     controllers.controller('ddsApp.controllers.BasisscholenCtrl',['$scope', 'ddsApp.services.ScholenSrvc', function($scope, ScholenSrvc){
+        $scope.basisschooltypes = [
+            {'label':'-- Alle basisscholen --','value':''},
+            {'label':'Kleuterscholen','value':'Kleuter'},
+            {'label':'Lagere scholen','value':'Lager'},
+            {'label':'Buitengewoon onderwijs','value':'Buitengewoon'},
+            {'label':'Kunstonderwijs','value':'KO'}
+        ];
+
+        $scope.form = {
+            basisschooltype: $scope.basisschooltypes[0]
+        };
+
         ScholenSrvc.loadData();
     }]);
 })();
