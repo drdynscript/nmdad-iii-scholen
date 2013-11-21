699526b29f85c668b0229a5c51a08bcb4ea21cbe
unknown <Administrator@DMARA12--8754W7.student.arteveldehs.be>
2013-11-21 11:02:15 +0100

    Update 21/11/2013 11:02

--- app/scripts/controllers/BasisscholenCtrl.js
+++ app/scripts/controllers/BasisscholenCtrl.js
@@ -3,7 +3,7 @@
 
     var controllers = angular.module('ddsApp.controllers');
 
-    controllers.controller('ddsApp.controllers.BasisscholenCtrl',['$scope', 'ddsApp.services.ScholenSrvc', function($scope, ScholenSrvc){
+    controllers.controller('ddsApp.controllers.BasisscholenCtrl',['$scope', '$filter', 'ddsApp.services.ScholenSrvc', function($scope, $filter, ScholenSrvc){
         $scope.basisschooltypes = [
             {'label':'-- Alle basisscholen --','value':''},
             {'label':'Kleuterscholen','value':'Kleuter'},
@@ -16,10 +16,17 @@
             basisschooltype: $scope.basisschooltypes[0]
         };
 
+        $scope.$watch('form.basisschooltype', function(newValue, oldValue){
+            var filteredBasisscholen = $filter('filter')($scope.basisscholen, {'aanbod':$scope.form.basisschooltype.value});
+
+            $('#info-message').html('<span class="badge pull-left">' + filteredBasisscholen.length + '</span>' + ' basisscholen');
+        });
+
         $scope.basisscholen = null;
 
         if(ScholenSrvc.getDataBasisscholen() !== null){
             $scope.basisscholen = ScholenSrvc.getDataBasisscholen();
+            $('#info-message').html('<span class="badge pull-left">' + $scope.basisscholen.length + '</span>' + ' basisscholen');
         }
 
         $scope.isList = true;
