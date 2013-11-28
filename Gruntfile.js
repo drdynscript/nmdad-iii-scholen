b3876b7669d37441ed1ad061068b05b1653faebb
drdynscript <phildp@arteveldehs.be>
2013-11-28 19:03:33 +0100

    Adding Grunt uglify --> read minify to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,24 @@ module.exports = function(grunt){
             my_js:{
                 src: ['app/scripts/**/*.js'], dest: 'dist/scripts/<%= pkg.name %>.js'
             }
+        },
+        uglify: {
+            options: {
+                mangle: false,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                  'dist/scripts/<%= pkg.name %>.min.js': ['<%= concat.my_js.dest %>']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //Default task to run --> command: grunt
-    grunt.registerTask('default', ['jshint', 'concat']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 }
